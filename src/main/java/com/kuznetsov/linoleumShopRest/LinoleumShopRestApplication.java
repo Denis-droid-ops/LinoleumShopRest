91b57cfbacfe74ac295e56bbbd653b6f16a5af28
Denis Kuznetsov <denis.denis.kuznecov@mail.ru>
2023-11-23 23:17:08 +0400

    Initial commit

---
+++ src/main/java/com/kuznetsov/linoleumShopRest/LinoleumShopRestApplication.java
@@ -0,0 +1,13 @@
+package com.kuznetsov.linoleumShopRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinoleumShopRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinoleumShopRestApplication.class, args);
+	}
+
+}
